e7a1f370099e7aa292747a07cee5b3304d9e0578
Maria Eduarda dos Santos Rosa <maria.e.santosr13@gmail.com>
2025-01-28 14:09:30 -0300

    commit

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost'; // ou '127.0.0.1'
+$username = 'root';  // seu nome de usuário do MySQL
+$password = '';      // sua senha do MySQL, se houver
+$dbname = 'login';   // nome correto do banco de dados, que é 'login'
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+?>
